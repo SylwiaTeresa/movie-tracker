64bdee62e8b6a7fc4f1833fb682c00c9283d1def
Sylwia Przystawska <sylwiateresap@gmail.com>
2026-06-14 20:50:22 +0200

    feat: add watched movie state handling

--- src/screens/MoviesScreen.tsx
+++ src/screens/MoviesScreen.tsx
@@ -1,12 +1,59 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
 
 export default function MoviesScreen() {
+  const [watchedMovies, setWatchedMovies] = useState<string[]>([]);
+
+  const movies = [
+    "Interstellar",
+    "Dune",
+    "Arrival",
+    "Masters of the Universe",
+  ]
+  const markAsWatched = (movie: string) => {
+    setWatchedMovies([...watchedMovies, movie]);
+    
+  }
   return (
-    <View>
-      <Text>⋆ Movies ⋆</Text>
-      <Text>Interstellar</Text>
-      <Text>Dune</Text>
-      <Text>Arrival</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>⋆ Movies ⋆</Text>
+
+      {movies.map((movie) => (
+        <View key={movie} style={styles.movieContainer}>
+          <Text style={styles.movie}>🎬 {movie} </Text>
+
+          {watchedMovies.includes(movie) ? (
+            <Text>✅ Watched</Text>
+          ) : (
+            <Pressable onPress={() => markAsWatched(movie)}>
+              <Text style={styles.button}>Mark as watched</Text>
+            </Pressable>
+          )}
+        </View>
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  movieContainer: {
+    marginBottom: 20,
+  },
+  movie: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  button: {
+    fontSize: 16,
+  },
+});
