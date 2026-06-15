c8249fe8f6cc1d813a1f3379e263ff5ec6680ade
Sylwia Przystawska <sylwiateresap@gmail.com>
2026-06-15 18:18:53 +0200

    feat: add tab navigation icons

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -2,12 +2,36 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import MoviesScreen from "../screens/MoviesScreen";
 import WatchedScreen from "../screens/WatchedScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
+
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home";
+          } else if (route.name === "Movies") {
+            iconName = "film";
+          } else {
+            iconName = "star";
+          }
+
+          return (
+            <Ionicons
+              name={iconName as any}
+              size={size}
+              color={color}
+            />
+          )
+        }
+      })}
+    >
       <Tab.Screen 
         name="Home"
         component={HomeScreen}
